28457f393cf207ea39c6fbd39e55be5043074b76
Eliurkis Diaz <eliurkis@gmail.com>
2022-07-16 15:29:31 -0400

    Refactor method for fields and params (#7)

--- src/PHPJasper.php
+++ src/PHPJasper.php
@@ -222,20 +222,33 @@ class PHPJasper
      * @return mixed
      * @throws Exception\InvalidInputFile
      */
-    public function listFields(string $input)
+    public function listFieldsAndParams(string $input)
     {
         if (!is_file($input)) {
             throw new Exception\InvalidInputFile();
         }
 
-        $fileContent = file_get_contents($input);
+        $objXmlDocument = simplexml_load_file($input);
+        $structure = json_decode(json_encode($objXmlDocument), true);
 
-        preg_match_all("/<field name=\"(.*?)\"/s", $fileContent, $fieldsKeys);
-        preg_match_all("/<subDataset name=\"(.*?)\"/s", $fileContent, $subDataSetKeys);
+        $fields = [];
+        foreach ($structure['field'] ?? [] as $field) {
+            $fields[] = $field['@attributes']['name'];
+        }
 
-        $matches = array_merge($fieldsKeys[1] ?? [], $subDataSetKeys[1] ?? []);
+        foreach ($structure['subDataset'] ?? [] as $subDataset) {
+            $fields[] = $subDataset['@attributes']['name'];
+        }
 
-        return $matches ?? [];
+        $params = [];
+        foreach ($structure['parameter'] ?? [] as $param) {
+            $params[] = $param['@attributes']['name'];
+        }
+
+        return [
+            'fields' => $fields,
+            'params' => $params,
+        ];
     }
 
     /**
